a2385fb497d43f7c80f4936abc6b5c2f391aee1b
lifefire1 <firend228@mail.ru>
2024-10-22 01:00:16 +0300

    feat(master) mvp

--- src/main/java/pdf/book/dao/Dictionary.java
+++ src/main/java/pdf/book/dao/Dictionary.java
@@ -1,6 +1,8 @@
 package pdf.book.dao;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
@@ -8,6 +10,7 @@ import lombok.Data;
 @Entity
 public class Dictionary {
   @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
   private String title;
   private String word;
